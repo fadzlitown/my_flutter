05fd8b4e563b9134d80439bfcd9d8d4f581659a6
mrazali <mohdfadzli.razali@sicpa.com>
2021-08-30 15:24:29 +0800

    - Remove the BlocProvider.value on specific screen since globally are using the same CounterTestCubit. EXCEPT THIRD SCREEN - In main, revert back the app to StatelessWidget & remove all the callbacks method - Generated Routing access - Updated app router class

--- lib/presentation/main.dart
+++ lib/presentation/main.dart
@@ -4,52 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_practices/logic/cubit_test/counter_test_cubit.dart';
 import 'package:flutter_practices/presentation/router/app_router.dart';
-import 'package:flutter_practices/presentation/screens/second_screen.dart';
-import 'package:flutter_practices/presentation/screens/third_screen.dart';
-
-import 'screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   ///StatefulWidget LIFECYCLE. #https://flutterbyexample.com/lesson/stateful-widget-lifecycle
   /// Flutter is instructed to build a StatefulWidget, it immediately calls createState(). This method must exist. like ==onCreate() android
-  @override
-  _MyAppState createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  /// note: if use case needs only 1 unique / SINGLE INSTANCE  for all the screens, then init below variableCubit
-  var _counterCubit = CounterTestCubit();
-  var _appRouter = AppRouter();
-
-
-  ///initState is called once and only once
-  /// suit for initialize data & properties that rely on this widget
-  @override
-  void initState() {
-    log("_MyAppState: initState");
-    super.initState();
-  }
-
-  ///The didChangeDependencies method is called immediately after initState on the first time the widget is built.
-  @override
-  void didChangeDependencies() {
-    log("_MyAppState: didChangeDependencies");
-  }
-
-  ///A MUST method is called often (UI renders). It is a required, @override and must return a Widget.
-  /// Remember that in Flutter all GUI is a widget with a child or children inside here!!
   @override
   Widget build(BuildContext context) {
     log("_MyAppState: build UI");
+    var _appRouter = AppRouter();
 
     //shortcut Bloc Alt + Enter
-    /// below telling flutter to have a single instance of Counter cubit to make it available inside the material widget
     return BlocProvider<CounterTestCubit>(
-      create: (context) => CounterTestCubit(),
+      /// below telling flutter to have a single instance of Counter cubit to make it available inside the material widget
+      ///
+      /// Defining here means it's GLOBALLY ACCESS across ALL OF SCREENS inside MATERIAL APP
+    create: (context) => CounterTestCubit(),
       child: MaterialApp(
         //Inside MaterialApp() provides the routes !
         title: 'Flutter Demo',
@@ -57,33 +31,11 @@ class _MyAppState extends State<MyApp> {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+
         ///route generator callback used when the app is navigated to a named route
         ///call method only w/o pass anything appRouter.onGenerateRoute
         onGenerateRoute: _appRouter.onGenerateRoute,
       ),
     );
-  }
-
-  /// this will called if the parent widget changes and has to rebuild this widget (because it needs to give it different data),
-  /// but it's being rebuilt with the same runtimeType, then this method is called.
-  @override
-  void didUpdateWidget(MyApp oldWidget) {
-    log("_MyAppState: didUpdateWidget");
-  }
-
-  /// setState = used to notify the "data has changed", & the widget at this build context should be rebuilt.
-  /// callback cannot be async, hence need to repaint the UI
-  @override
-  void setState(VoidCallback fn) {
-    log("_MyAppState: setState");
-  }
-
-  /// dispose is called when the State object is removed, which is permanent.
-  /// This method is where to unsubscribe and cancel all animations, streams, etc.
-  @override
-  void dispose() {
-    log("_MyAppState: dispose");
-    _appRouter.dispose();
-    super.dispose();
   }
 }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -7,35 +7,31 @@ import 'package:flutter_practices/presentation/screens/second_screen.dart';
 import 'package:flutter_practices/presentation/screens/third_screen.dart';
 
 class AppRouter {
+  /// USE this CUBIT/BLOC instance below if you want to provde to a SINGLE / MORE screen or any SPECIFIC SCREENS
+  /// by using BlocProvider.value(value: _counterCubit, child..) on the SCREEN WIDGET
   var _counterCubit = CounterTestCubit();
 
   Route onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
 
-      /// (_) identifier meaning it's private to its library.
+        /// (_) identifier meaning it's private to its library.
         return MaterialPageRoute(builder: (_) {
           /// BlocProvider.value(..) -> Takes a [value] and a [child] which will have access to the [value] via `BlocProvider.of(context)`, & close manually
           /// note: if needs only 1 unique instance for all the screens, then use BlocProvider.value(value: _counterCubit, child..), hence it kept the states
           ///
           /// BlocProvider(create..) -> Takes a [Create] function that is responsible for creating the [Bloc] or [Cubit] & close automatically
           /// BlocProvider(create: (context) => CounterTestCubit(), ..) -> Always create a new instance when its called . hence new states expected
-          return BlocProvider.value(
-            value: _counterCubit,
-            child: HomeScreen(title: 'MY FLUTTER APP', color: Colors.blueGrey),
-          );
+          return HomeScreen(title: 'MY FLUTTER APP', color: Colors.blueGrey);
         });
         break;
       case '/2':
 
         /// (_) identifier meaning it's private to its library.
         return MaterialPageRoute(builder: (_) {
-          return BlocProvider.value(
-            value: _counterCubit,
-            child: SecondScreen(
-              title: 'Second Screen',
-              colorApp: Colors.red,
-            ),
+          return SecondScreen(
+            title: 'Second Screen',
+            colorApp: Colors.red,
           );
         });
         break;
@@ -52,9 +48,5 @@ class AppRouter {
       default:
         return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
