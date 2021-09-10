a55e61a2deee4e41ff9f80641a148a7bc1270190
mrazali <mohdfadzli.razali@sicpa.com>
2021-09-10 17:33:53 +0800

    Debugging inside local class

--- lib/logic/cubit_test/counter_test_cubit.dart
+++ lib/logic/cubit_test/counter_test_cubit.dart
@@ -6,7 +6,7 @@ import 'package:meta/meta.dart';
 
 part 'counter_test_state.dart';
 
-class CounterTestCubit extends Cubit<CounterTestState> with HydratedMixin {
+class CounterTestCubit extends Bloc<CounterTestState> with HydratedMixin {
   // final InternetCubit internetCubit;
 
   CounterTestCubit()
@@ -30,8 +30,10 @@ class CounterTestCubit extends Cubit<CounterTestState> with HydratedMixin {
     return emit(CounterTestState(counterValue: 0, wasIncremented: false));
   }
 
+  /// by triggering multiply() there's no crashing or error screen
   void multiply() {
     int total = state.counterValue * 2;
+    addError(Exception("Could not write to storate!"), StackTrace.current);
     return emit(CounterTestState(
         counterValue: state.counterValue,
         wasIncremented: true,
@@ -39,11 +41,26 @@ class CounterTestCubit extends Cubit<CounterTestState> with HydratedMixin {
   }
 
   //every time trigger func / event & state update (rebuild UI)  this callback will be called
-  /// NOTED !!!! ISSUE FOUND IN PACKAGE DEPENDENCY
-  /// cannot used log() if implement with HydratedMixin
+  /// A change occurs when a new state is emitted. onChange is called before the state of the cubit is updated.
+  /// onChange is a great spot to add logging/analytics for a specific cubit
+  @override
+  void onChange(Change<CounterTestState> change) {
+    print("CounterTestCubit: onChange");
+    super.onChange(change);
+  }
+
+  ///Called whenever an error occurs within a Cubit.
+  ///By default all errors will be ignored and Cubit functionality will be unaffected
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print("$error, $stackTrace");
+    super.onError(error, stackTrace);
+  }
+
+  ///This debug method will be available only if this class extends to Bloc
   // @override
-  // void onChange(Change<CounterTestState> change) {
-  //   log("CounterTestCubit: onChange");
+  // void onTransition(Transition<Event, State> transition) {
+  //   super.onTransition(transition);
   // }
 
   @override
